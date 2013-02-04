c522318a515d6417d5a6dba8837b5f6c64b687a4
Miles Johnson <mileswjohnson@gmail.com>
2013-02-04 15:46:22 -0800

    Added BaseInstallShell

---
+++ Console/Command/BaseInstallShell.php
@@ -0,0 +1,556 @@
+<?php
+/**
+ * @copyright	Copyright 2006-2013, Miles Johnson - http://milesj.me
+ * @license		http://opensource.org/licenses/mit-license.php - Licensed under the MIT License
+ * @link		http://milesj.me/code/cakephp/utility
+ */
+
+Configure::write('debug', 2);
+Configure::write('Cache.disable', true);
+
+App::uses('ConnectionManager', 'Model');
+App::uses('Validation', 'Utility');
+App::uses('AppShell', 'Console/Command');
+
+abstract class BaseInstallShell extends AppShell {
+
+	/**
+	 * Runtime configuration.
+	 *
+	 * @var array
+	 */
+	public $config = array();
+
+	/**
+	 * DB Instance.
+	 *
+	 * @var DboSource
+	 */
+	public $db;
+
+	/**
+	 * Database config to use.
+	 *
+	 * @var string
+	 */
+	public $dbConfig;
+
+	/**
+	 * Tables required for installation to proceed.
+	 *
+	 * @var array
+	 */
+	public $requiredTables = array();
+
+	/**
+	 * Steps to run through.
+	 *
+	 * @var array
+	 */
+	public $steps = array();
+
+	/**
+	 * Table prefix.
+	 *
+	 * @var string
+	 */
+	public $tablePrefix;
+
+	/**
+	 * Single user record.
+	 *
+	 * @var array
+	 */
+	public $user;
+
+	/**
+	 * Mapping of user fields to grab during user creation.
+	 *
+	 * @var array
+	 */
+	public $userFields = array(
+		'username' => 'username',
+		'password' => 'password',
+		'email' => 'email'
+	);
+
+	/**
+	 * Users model.
+	 *
+	 * @var string
+	 */
+	public $usersModel = 'User';
+
+	/**
+	 * Users database table.
+	 *
+	 * @var string
+	 */
+	public $usersTable = 'users';
+
+	/**
+	 * Execute installer and cycle through all steps.
+	 *
+	 * @return void
+	 */
+	public function main() {
+		$this->hr(1);
+		$this->out('Installation Steps:');
+
+		$counter = 1;
+
+		foreach ($this->steps as $method) {
+			$this->steps($counter);
+
+			if (!call_user_func(array($this, $method))) {
+				$this->err('<error>Installation aborted!</error>');
+				break;
+			}
+
+			$counter++;
+		}
+	}
+
+	/**
+	 * Check the database status before installation.
+	 *
+	 * @return bool
+	 */
+	public function checkDbConfig() {
+		if (!$this->dbConfig || !$this->db) {
+			$dbConfigs = array_keys(get_class_vars('DATABASE_CONFIG'));
+
+			$this->out('Available database configurations:');
+
+			foreach ($dbConfigs as $i => $db) {
+				$this->out('[' . $i . '] ' . $db);
+			}
+
+			$this->out();
+
+			$answer = $this->in('<question>Which database should the tables be created in?</question>', array_keys($dbConfigs));
+
+			if (isset($dbConfigs[$answer])) {
+				$this->setDbConfig($dbConfigs[$answer]);
+			} else {
+				$this->checkDbConfig();
+			}
+		}
+
+		$this->out(sprintf('You have chosen the database: %s', $this->dbConfig));
+
+		$answer = strtoupper($this->in('<question>Is this correct?</question>', array('Y', 'N')));
+
+		if ($answer === 'N') {
+			$this->dbConfig = null;
+			$this->checkDbConfig();
+		}
+
+		// Check that database is connected
+		if (!$this->db->isConnected()) {
+			$this->err(sprintf('<error>Database connection for %s failed!</error>', $this->dbConfig));
+			return false;
+		}
+
+		$this->out('<info>Database check successful, proceeding...</info>');
+		return true;
+	}
+
+	/**
+	 * Check that all the required tables exist in the database.
+	 *
+	 * @return bool
+	 */
+	public function checkRequiredTables() {
+		if ($this->requiredTables) {
+			$this->out(sprintf('The following tables are required: %s', implode(', ', $this->requiredTables)));
+			$this->out('<info>Checking tables...</info>');
+
+			$tables = $this->db->listSources();
+			$missing = array();
+
+			foreach ($this->requiredTables as $table) {
+				if (!in_array($table, $tables)) {
+					$missing[] = $table;
+				}
+			}
+
+			if ($missing) {
+				$this->err(sprintf('<error>Missing tables %s; installation can not proceed</error>', implode(', ', $missing)));
+				return false;
+			}
+		}
+
+		$this->out('<info>Table status good, proceeding...</info>');
+		return true;
+	}
+
+	/**
+	* Set the table prefix to use.
+	*
+	* @return bool
+	*/
+	public function checkTablePrefix() {
+		if (!$this->tablePrefix) {
+			$this->setTablePrefix($this->in('<question>What table prefix would you like to use?</question>'));
+		}
+
+		$this->out(sprintf('You have chosen the prefix: %s', $this->tablePrefix));
+
+		$answer = strtoupper($this->in('<question>Is this correct?</question>', array('Y', 'N')));
+
+		if ($answer === 'N') {
+			$this->tablePrefix = null;
+			$this->checkTablePrefix();
+		}
+
+		$this->out('<info>Table prefix set, proceeding...</info>');
+		return true;
+	}
+
+	/**
+	 * Determine the users model to use.
+	 *
+	 * @return bool
+	 */
+	public function checkUsersModel() {
+		if (!$this->usersModel) {
+			$this->setUsersModel($this->in('<question>What is the name of your users model?</question>'));
+		}
+
+		$this->out(sprintf('You have chosen the model: %s', $this->usersModel));
+
+		$answer = strtoupper($this->in('<question>Is this correct?</question>', array('Y', 'N')));
+
+		if ($answer === 'N') {
+			$this->usersModel = null;
+			$this->checkUsersModel();
+		}
+	}
+
+	/**
+	 * Determine the users table to use.
+	 *
+	 * @return bool
+	 */
+	public function checkUsersTable() {
+		if (!$this->usersTable) {
+			$this->setUsersTable($this->in('<question>What is the name of your users table?</question>'));
+		}
+
+		$this->out(sprintf('You have chosen the table: %s', $this->usersTable));
+
+		$answer = strtoupper($this->in('<question>Is this correct?</question>', array('Y', 'N')));
+
+		if ($answer === 'N') {
+			$this->usersTable = null;
+			$this->checkUsersTable();
+		} else {
+			$this->setRequiredTables(array($this->usersTable));
+		}
+
+		$this->checkUsersModel();
+
+		$this->out('<info>Users table set, proceeding...</info>');
+		return true;
+	}
+
+	/**
+	 * Create the database tables based off the schemas.
+	 *
+	 * @return bool
+	 */
+	public function createTables() {
+		$answer = strtoupper($this->in('<question>Existing tables will be deleted, continue?</question>', array('Y', 'N')));
+
+		if ($answer === 'N') {
+			return false;
+		}
+
+		$schemas = glob(CakePlugin::path($this->plugin) . '/Config/Schema/*.sql');
+		$executed = 0;
+		$total = count($schemas);
+		$tables = array();
+
+		foreach ($schemas as $schema) {
+			$contents = file_get_contents($schema);
+			$contents = String::insert($contents, array('prefix' => FORUM_PREFIX), array('before' => '{', 'after' => '}'));
+			$contents = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $contents);
+
+			$queries = explode(';', $contents);
+			$tables[] = $this->tablePrefix . str_replace('.sql', '', basename($schema));
+
+			foreach ($queries as $query) {
+				$query = trim($query);
+
+				if ($query !== '' && $this->db->execute($query)) {
+					$command = trim(substr($query, 0, strpos($query, ' ')));
+
+					if ($command === 'CREATE' || $command === 'ALTER') {
+						$executed++;
+					}
+				}
+			}
+		}
+
+		if ($executed != $total) {
+			$this->out('<error>Failed to create database tables!</error>');
+			$this->out('Rolling back and dropping any created tables');
+
+			foreach ($tables as $table) {
+				$this->db->execute(sprintf('DROP TABLE `%s`;', $table));
+			}
+
+			return false;
+		}
+
+		$this->out('<info>Tables created successfully, proceeding...</info>');
+		return true;
+	}
+
+	/**
+	 * Gather all the data for creating a new user.
+	 *
+	 * @return int
+	 */
+	public function createUser() {
+		$data = array();
+
+		foreach ($this->userFields as $base => $field) {
+			$data[$field] = $this->getFieldInput($base);
+		}
+
+		$model = ClassRegistry::init($this->usersModel);
+		$model->create();
+		$model->save($data, false);
+
+		$this->config = $data;
+		$this->user = $model->find('first', array(
+			'conditions' => array($model->alias . '.' . $model->primaryKey => $model->id),
+			'recursive' => -1
+		));
+
+		return $model->id;
+	}
+
+	/**
+	 * Find a user within the users table.
+	 *
+	 * @return int
+	 */
+	public function findUser() {
+		$model = ClassRegistry::init($this->usersModel);
+		$id = trim($this->in('<question>User ID:</question>'));
+
+		if (!$id) {
+			$this->out('<error>Invalid ID, please try again</error>');
+			return $this->findUser();
+		}
+
+		$result = $model->find('first', array(
+			'conditions' => array($model->alias . '.' . $model->primaryKey => $id),
+			'recursive' => -1
+		));
+
+		if (!$result) {
+			$this->out('<error>User does not exist, please try again</error>');
+			return $this->findUser();
+		}
+
+		$this->user = $result;
+
+		return $id;
+	}
+
+	/**
+	 * Get the value of an input.
+	 *
+	 * @param string $field
+	 * @return string
+	 */
+	public function getFieldInput($field) {
+		$model = ClassRegistry::init($this->usersModel);
+
+		switch ($field) {
+			case 'username':
+				$username = trim($this->in('<question>Username:</question>'));
+
+				if (!$username) {
+					$username = $this->getFieldInput($field);
+
+				} else {
+					$result = $model->find('count', array(
+						'conditions' => array($model->alias . '.' . $this->userFields['username'] => $username)
+					));
+
+					if ($result) {
+						$this->out('<error>Username already exists, please try again</error>');
+						$username = $this->getFieldInput($field);
+					}
+				}
+
+				return $username;
+			break;
+
+			case 'email':
+				$email = trim($this->in('<question>Email:</question>'));
+
+				if (!$email) {
+					$email = $this->getFieldInput($field);
+
+				} else if (!Validation::email($email)) {
+					$this->out('<error>Invalid email address, please try again</error>');
+					$email = $this->getFieldInput($field);
+
+				} else {
+					$result = $model->find('count', array(
+						'conditions' => array($model->alias . '.' . $this->userFields['email'] => $email)
+					));
+
+					if ($result) {
+						$this->out('<error>Email already exists, please try again</error>');
+						$email = $this->getFieldInput($field);
+					}
+				}
+
+				return $email;
+			break;
+
+			// Password, others...
+			default:
+				$value = trim($this->in(sprintf('<question>%s:</question>', Inflector::humanize($field))));
+
+				if (!$value) {
+					$value = $this->getFieldInput($field);
+				}
+
+				return $value;
+			break;
+		}
+	}
+
+	/**
+	 * Set the database config to use and load the data source.
+	 *
+	 * @param string $config
+	 * @return BaseInstallShell
+	 */
+	public function setDbConfig($config) {
+		$this->dbConfig = $config;
+		$this->db = ConnectionManager::getDataSource($config);
+
+		return $this;
+	}
+
+	/**
+	 * Set the required tables.
+	 *
+	 * @param array $tables
+	 * @return BaseInstallShell
+	 */
+	public function setRequiredTables(array $tables) {
+		$this->requiredTables = array_unique(array_merge($this->requiredTables, $tables));
+
+		return $this;
+	}
+
+	/**
+	 * Set the list of steps to trigger during install.
+	 * Step key is the step name and the value is the method to execute.
+	 *
+	 * @param array $steps
+	 * @return BaseInstallShell
+	 */
+	public function setSteps(array $steps) {
+		$this->steps = $steps;
+
+		return $this;
+	}
+
+	/**
+	 * Set the table prefix to use.
+	 *
+	 * @param string $prefix
+	 * @return BaseInstallShell
+	 */
+	public function setTablePrefix($prefix) {
+		if ($prefix) {
+			$prefix = trim($prefix, '_') . '_';
+		}
+
+		$this->tablePrefix = $prefix;
+
+		return $this;
+	}
+
+	/**
+	 * Set the user fields mapping.
+	 *
+	 * @param array $fields
+	 * @return BaseInstallShell
+	 */
+	public function setUserFields(array $fields) {
+		$clean = array();
+
+		foreach ($fields as $base => $field) {
+			if (is_numeric($base)) {
+				$base = $field;
+			}
+
+			$clean[$base] = $field;
+		}
+
+		$this->userFields = $clean;
+
+		return $this;
+	}
+
+	/**
+	 * Set the users model.
+	 *
+	 * @param string $model
+	 * @return BaseInstallShell
+	 */
+	public function setUsersModel($model) {
+		$this->usersModel = trim($model);
+
+		return $this;
+	}
+
+	/**
+	 * Set the users table.
+	 *
+	 * @param string $table
+	 * @return BaseInstallShell
+	 */
+	public function setUsersTable($table) {
+		$this->usersTable = trim($table);
+
+		return $this;
+	}
+
+	/**
+	 * Table of contents.
+	 *
+	 * @param int $step
+	 * @return void
+	 */
+	public function steps($step = 0) {
+		$this->hr(1);
+
+		$counter = 1;
+
+		foreach ($this->steps as $title => $method) {
+			if ($counter < $step) {
+				$this->out('[x] ' . $title);
+			} else {
+				$this->out('[' . $counter . '] <comment>' . $title . '</comment>');
+			}
+
+			$counter++;
+		}
+
+		$this->out();
+	}
+
+}
